5850a590d117d6b0d658350d7a77b78d54f6f246
daisy@wk <daisykang@sina.com>
2020-06-25 22:54:35 +1200

    inital

--- src/router/config.js
+++ src/router/config.js
@@ -6,7 +6,6 @@ const dashboardRoutes = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    rtlName: "لوحة القيادة",
     icon: Dashboard,
     component: DashboardPage,
     layout: "/admin"
